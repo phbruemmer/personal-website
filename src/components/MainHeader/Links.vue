56758d320bf8dc8950193360842ee23545cde08f
phbruemmer <phbruemmer@gmail.com>
2025-02-06 20:32:58 +0100

    - Added underline animation for navbar links

--- src/components/MainHeader/Links.vue
+++ src/components/MainHeader/Links.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="link-container">
-    <a :href="reference"><slot /></a>
+    <a :href="reference" @click.prevent="scrollToTarget"><slot /></a>
   </div>
 </template>
 
@@ -13,16 +13,49 @@ export default {
       required: true,
     },
   },
+
+  methods: {
+    scrollToTarget() {
+      const element = document.querySelector(this.reference);
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 a {
+  font-size: 16px;
+  font-weight: 100;
+  font-family: Arial, Helvetica, sans-serif;
+  display: inline-block;
+  position: relative;
   text-decoration: none;
   color: #000;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: 100;
-  font-size: 16px;
-  margin: 10px;
+}
+
+a::after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+  height: 2px;
+  background-color: #000;
+  transition: width 0.2s ease;
+}
+
+a:hover::after {
+  width: 100%;
+}
+
+a:hover {
+  cursor: pointer;
 }
 </style>
