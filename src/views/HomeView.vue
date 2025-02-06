74b6180fa847eb27570b1d4dccb90c3472241467
phbruemmer <phbruemmer@gmail.com>
2025-02-06 20:26:33 +0100

    - Added background image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,3 +10,24 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+center {
+  background-image: url("@/assets/image.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+/* Style the heading */
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+</style>
