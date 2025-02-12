c9a49ce13e59a1ae71b3d6167f55e3f75b71072d
phbruemmer <phbruemmer@gmail.com>
2025-02-12 18:48:39 +0100

    - Modified About page.

---
+++ src/components/AboutPage/FrontPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="center">
+      <h1>~ About ~</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontPage",
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 64px;
+  color: #fff;
+}
+
+.center {
+  margin-top: 2vh;
+  background-image: url("@/assets/About/code.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 40vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("@/assets/About/code.png");
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2), 0 -10px 20px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.3s ease-in-out;
+}
+
+.center:hover {
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.4), 0 -10px 20px rgba(0, 0, 0, 0.4);
+}
+
+h1 {
+  transition: transform 0.3s ease-in-out;
+}
+
+h1:hover {
+  cursor: default;
+  transform: scale(1.05);
+}
+
+.container {
+  position: relative;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,17 @@
 <template>
   <DefaultHeader />
+  <FrontPage />
 </template>
 
 <script>
 import DefaultHeader from "@/components/DefaultHeader/DefaultHeader.vue";
+import FrontPage from "@/components/AboutPage/FrontPage.vue";
 
 export default {
   name: "AboutView",
   components: {
     DefaultHeader,
+    FrontPage,
   },
 };
 </script>
